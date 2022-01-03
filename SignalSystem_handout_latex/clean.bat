fa98ba8e4e2422a90d8041c49fe9b9f3aef38246
liubo <geniusrabbit@qq.com>
2022-01-03 16:20:01 +0800

    信号系统笔记构建模板

---
+++ SignalSystem_handout_latex/clean.bat
@@ -0,0 +1,3 @@
+del /F *.log *.xml *.bcf *.ptc *.aux *.thm *.glo *.dvi *.gls *.idx *.ilg *.ind *.toc *.lot *.lof *.bbl *.blg *.out *.bak *.ps *.synctex *.synctex.gz *.txt
+
+del /S *.aux *.bbl *.blg *.log *.bak
